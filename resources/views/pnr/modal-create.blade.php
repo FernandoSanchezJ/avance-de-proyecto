45f4fda9c836137b6a307e0f4a55ec45ae3a9b5f
Fernando ! <tailonsanchez900@gmail.com>
2023-12-09 07:19:15 -0600

    Dump

--- resources/views/pnr/modal-create.blade.php
+++ resources/views/pnr/modal-create.blade.php
@@ -8,7 +8,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
 
-      <form action=" {{ route('home.store') }}" method="post" enctype="multipart/form-data">
+      <form action=" {{ route('pnrs.store') }}" method="post" enctype="multipart/form-data">
 @csrf
           <div class="modal-body">
               
